20275326b9fb8d100fbb75e07a9b1fb1a7105d41
Prince Joseph Esteves <74764886+Nyanta2598@users.noreply.github.com>
2022-11-22 09:16:52 +0800

    minor adjustment

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
             <p>{data.name}</p>
           </div>
           <div className='temp'>
-            {data.main ? <h1>{data.main.temp}</h1> : null}
+            {data.main ? <h1>{data.main.temp}°K</h1> : null}
           </div>
           <div className='description'>
             {data.weather ? <p>{data.weather[0].description}</p>: null}
@@ -24,15 +24,15 @@ function App() {
         </div>
         <div className='bottom'>
           <div className='feels'>
-            {data.main ? <p className='bold'>{data.main.feels_like}</p> : null}
+            {data.main ? <p className='bold'>{data.main.feels_like}°K</p> : null}
             <p>Feels Like</p>
           </div>
           <div className='humidity'>
-          {data.main ? <p className='bold'>{data.main.humidity}</p> : null}
+          {data.main ? <p className='bold'>{data.main.humidity}%</p> : null}
             <p>Humidity</p>
           </div>
           <div className='wind'>
-          {data.wind ? <p className='bold'>{data.wind[0].speed}</p>: null}
+          {data.wind ? <p className='bold'>{data.wind.speed} MPH</p>: null}
             <p>Wind Speed</p>
           </div>
         </div>
